74375f8c84fcf7b8ab2a316832f935f1ff8583c2
MatthewSamaha <matthewsamaha@gmail.com>
2026-06-30 14:46:53 -0400

    docs

--- BrainlessGameJam/src/com/lobsterchops/brainlessgamejam/util/Timer.java
+++ BrainlessGameJam/src/com/lobsterchops/brainlessgamejam/util/Timer.java
@@ -1,6 +1,7 @@
 package com.lobsterchops.brainlessgamejam.util;
 
 // Timer class for tracking elapsed time
+// Timer is a utility class that allows you to set a duration and check if the timer has finished based on the elapsed time since it was started.
 public class Timer {
 	
 	private long duration;
